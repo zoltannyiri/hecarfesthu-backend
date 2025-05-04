b826b7a34696013c719c39af064927642bc13221
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-05-05 01:03:53 +0200

    asd

--- src/server.ts
+++ src/server.ts
@@ -228,7 +228,7 @@ const authDrive = new googleApis.auth.GoogleAuth({
       type: "service_account",
       project_id: "hecarfest-vip",
       private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       client_id: process.env.GOOGLE_CLIENT_ID,
     },

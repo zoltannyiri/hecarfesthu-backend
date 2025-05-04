feaa7735fef229804fbf7363689306e17e9bbde9
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-05-05 00:56:00 +0200

    teszt

--- src/server.ts
+++ src/server.ts
@@ -225,12 +225,12 @@ async function sendEmail(to: string, subject: string, html: string): Promise<voi
 
 const authDrive = new googleApis.auth.GoogleAuth({
     credentials: {
-        type: "service_account",
-        project_id: "hecarfest-vip",
-        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_CLIENT_ID,
+      type: "service_account",
+      project_id: "hecarfest-vip",
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
     },
     scopes: ['https://www.googleapis.com/auth/drive']
   });
